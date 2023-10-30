09afe287d7c273ad9356ed44f21ac9f89c9d2af5
jaimergp <jaimergp@users.noreply.github.com>
2023-10-30 14:12:45 +0100

    vendor mamba.utils 1.5.2

--- conda_forge_feedstock_check_solvable/mamba_solver.py
+++ conda_forge_feedstock_check_solvable/mamba_solver.py
@@ -36,7 +36,7 @@ import conda_build.api
 import conda_package_handling.api
 
 import libmambapy as api
-from mamba.utils import load_channels
+from .mamba_utils import load_channels
 
 from conda_build.conda_interface import pkgs_dirs
 from conda_build.utils import download_channeldata
