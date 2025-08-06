dd9e012f7643b9ca25b008f5701b2b8cf35ce410
beckermr <becker.mr@gmail.com>
2025-08-06 06:43:15 -0500

    fix: remove defaults from testing

--- conda_forge_feedstock_check_solvable/check_solvable.py
+++ conda_forge_feedstock_check_solvable/check_solvable.py
@@ -218,7 +218,7 @@ def _is_recipe_solvable_on_platform(
             # channel_sources might be part of some zip_key
             channel_sources.extend([c.strip() for c in source.split(",")])
     else:
-        channel_sources = ["conda-forge", "defaults"]
+        channel_sources = ["conda-forge"]
 
     if "msys2" not in channel_sources and platform.startswith("win"):
         channel_sources.append("msys2")
